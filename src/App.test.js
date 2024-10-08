a8d01c809b3d0f931f316c9ac0534d5b247d7c36
avecespasaux <avecespasa13@gmail.com>
2024-10-08 19:11:39 +0200

    cambios

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import Booking from './Componentes/Booking';
-
-test('Renders the BookingForm heading', () => {
-    render(<Booking />);
-    const headingElement = screen.getByText("Book Now");
-    expect(headingElement).toBeInTheDocument();
-})
--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+///import 'modern-normalize/modern-normalize.css';
 import './index.css';
+import ScrollToTop from "./components/layout/ScrollToTop";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />
+      <ScrollToTop />
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
